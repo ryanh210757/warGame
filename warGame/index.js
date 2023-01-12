8bc2f685cbf18f3b1b75bb65190a91d174ba5813
ryanh210757 <kryanharris1@gmail.com>
2023-01-12 15:08:29 -0600

    add styling

--- warGame/index.js
+++ warGame/index.js
@@ -11,3 +11,17 @@ function handleClick(){
 }
 
 document.getElementById("btn").addEventListener("click", handleClick)
+
+
+
+document.getElementById("drawBtn").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckID}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+                <img src=${data.cards[1].image} class="card" />
+            `
+        })
+})
